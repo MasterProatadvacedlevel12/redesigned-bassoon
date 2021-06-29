9335f421ea1635b7a49147f814bfcd186a76eefa
MasterProatadvacedlevel12 <74993217+MasterProatadvacedlevel12@users.noreply.github.com>
2021-06-29 15:48:26 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+function  preload() {
+    mustache = loadImage("https://i.postimg.cc/3x3QzSGq/m.png");
+}
+
+
+function setup(){
+canvas = createCanvas(300,300);
+canvas.center();
+video = createCapture(VIDEO);
+video.size(300,300);
+video.hide();
+poseNet = ml5.poseNet(video, loaded);
+poseNet.on('pose', gotPoses);
+}
+
+
+function loaded(){
+    console.log("poseNet model is loaded");
+}
+
+nosex=0;
+
+nosey=0;
+
+function gotPoses(results){
+    if(results.length >0){
+        console.log(results);
+        console.log("Nose x axis"+ results[0].pose.nose.x);
+        console.log("Nose y axis"+ results[0].pose.nose.y);
+        nosex=results[0].pose.nose.x - 13;
+        nosey=results[0].pose.nose.y + 4;
+    }
+}
+
+function draw(){
+    image(video, 0, 0, 300, 300);
+    image(mustache, nosex, nosey, 30, 30);
+}
+
+function take_snapshot(){
+    save("filterimage.jpg");
+}
